b5dfb4d6bb6f575b5412a423059e509fe0ecd13e
NaityHS <baloonsnaity@gmail.com>
2026-02-12 16:15:20 -0300

    Delete OnionwareVsHomer.lua

--- OnionwareVsHomer.lua
+++
@@ -1,3 +0,0 @@
--- hello skid, onionware is down and this is just a kick message
-local player = game.Players.LocalPlayer
-player:Kick("Onionware is currently offline. We are trying everything we can to get the script back online as quickly as possible! - naity")
