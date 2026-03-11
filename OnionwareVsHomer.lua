c0b9805885752cf6131e7cdd084b790644943c6d
NaityHS <baloonsnaity@gmail.com>
2026-03-11 13:33:45 -0300

    Update kick message for Onionware script

--- OnionwareVsHomer.lua
+++ OnionwareVsHomer.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Onionware is currently offline. We are trying everything we can to get the script back online as quickly as possible! - naity")
+game.Players.LocalPlayer:Kick("Onionware for YvsH is currently DISCONTINUED. ")
